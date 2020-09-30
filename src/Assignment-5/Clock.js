b16f426d4ae40f4c9b19ddeb205ad156b464d6d8
nokaur <71750548+nokaur@users.noreply.github.com>
2020-09-30 20:24:55 +0530

    Changes-4

--- src/Assignment-5/Clock.js
+++ src/Assignment-5/Clock.js
@@ -2,7 +2,58 @@ import React, { Component } from "react";
 import icon from "../Assignment-5/images2.png";
 import backgroundicon from "../Assignment-5/images (1).jpg";
 import "../Assignment-5/MyStyleSheet.css";
-import { ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  Dimensions
+} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: "170px",
+    textAlign: "center",
+    marginLeft: "60px",
+    height: "180px",
+    //backgroundColor: "gray"
+    //backgroundColor: "rgb(0,0,0)"
+    backgroundColor: "rgba(0,0,0,0.1)"
+  },
+  textStyle: {
+    fontSize: 18,
+    color: "#00bfff",
+    fontWeight: "bold",
+    marginTop: 55
+  },
+  subText: {
+    fontSize: 13,
+    color: "#00bfff",
+    fontWeight: "bold",
+    marginTop: 10
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.1)"
+  }
+  //   avatarStyle: {
+  //     width: 50,
+  //     height: 100,
+  //     marginTop: 0,
+  //     borderRadius: 20,
+  //     alignSelf: "center"
+  //   },
+  //   textStyle: {
+  //     marginTop: 60,
+  //     fontSize: 18,
+  //     marginBottom: 22,
+  //     color: "#00bfff",
+  //     fontWeight: "bold"
+  //   },
+  //   balanceContainer: {
+  //     padding: 2
+  //   }
+});
 
 class Clock extends Component {
   constructor(props) {
@@ -16,7 +67,8 @@ class Clock extends Component {
             <tr
               style={{
                 backgroundColor: "black",
-                color: "white"
+                color: "white",
+                height: "50px"
               }}
             >
               <td>
@@ -30,7 +82,19 @@ class Clock extends Component {
             </tr>
             <tr style={{ height: "200px" }}>
               <td>
-                <ImageBackground source={{ url:'backgroundicon'}}/>
+                <ImageBackground
+                  source={{
+                    uri: `${backgroundicon}`
+                  }}
+                  style={styles.container}
+                >
+                  <View style={styles.overlay}>
+                    <Text style={styles.textStyle}>CLOCK</Text>
+                  </View>
+                  <View style={[styles.overlay, { height: "101.5px" }]}>
+                    <Text style={styles.subText}>DATE</Text>
+                  </View>
+                </ImageBackground>
               </td>
             </tr>
           </table>
